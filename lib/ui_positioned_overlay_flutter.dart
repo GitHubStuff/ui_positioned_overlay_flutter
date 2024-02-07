a9bb37b085174bf2ed09178767a6ed4c8e830af8
steven.smith <steven.smith19@accesscfa.com>
2024-02-07 16:38:32 -0500

    Fixed - working animation

--- lib/ui_positioned_overlay_flutter.dart
+++ lib/ui_positioned_overlay_flutter.dart
@@ -1,24 +1,23 @@
 library ui_positioned_overlay_flutter;
 
 import 'package:flutter/material.dart';
-import 'package:ui_animated_opacity/ui_animated_opacity.dart';
 
 class PositionedOverlayWidget<T> extends StatefulWidget {
-  final UIOpacityCubit? opacityCubit;
   final GlobalKey triggerKey;
-  final Offset offset;
   final Color backgroundColor;
-  final double opacity;
+  final double layerOpacity;
+  final Duration fadeDuration;
+  final Offset offset;
   final Widget Function(
       BuildContext context, void Function([dynamic result]) dismiss) builder;
 
   const PositionedOverlayWidget({
     super.key,
     required this.triggerKey,
-    this.opacityCubit,
     this.offset = const Offset(0, 0),
     this.backgroundColor = Colors.black,
-    this.opacity = 0.2,
+    this.layerOpacity = 0.2,
+    this.fadeDuration = const Duration(milliseconds: 300),
     required this.builder,
   });
 
@@ -27,60 +26,86 @@ class PositionedOverlayWidget<T> extends StatefulWidget {
       _PositionedOverlayWidget<T>();
 }
 
-class _PositionedOverlayWidget<T> extends State<PositionedOverlayWidget<T>> {
-  OverlayEntry? _overlayEntry;
+class _PositionedOverlayWidget<T> extends State<PositionedOverlayWidget<T>>
+    with TickerProviderStateMixin {
+  OverlayEntry? overlayEntry;
+  late final AnimationController controller;
+  late final Animation<double> animation;
+  T? poppedResult;
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback(_updatePosition);
+    controller = AnimationController(
+      vsync: this,
+      duration: widget.fadeDuration,
+    );
+    controller.addStatusListener((status) {
+      if (AnimationStatus.dismissed == status) {
+        Navigator.of(context)
+            .pop(poppedResult == null ? null : poppedResult as T);
+      }
+    });
+    animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeInOut,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
+        /// Tap on the background to dismiss the overlay
         return GestureDetector(
           behavior: HitTestBehavior.translucent,
-          onTap: () {
-            _pop();
-          },
+          onTap: () => _pop(),
           child: Scaffold(
-            backgroundColor: widget.backgroundColor.withOpacity(widget.opacity),
+            backgroundColor:
+                widget.backgroundColor.withOpacity(widget.layerOpacity),
           ),
         );
       },
     );
   }
 
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   void _pop([dynamic result]) {
-    Duration popDelay = const Duration();
-    if (widget.opacityCubit != null) {
-      popDelay = widget.opacityCubit!.fadeDuration;
-      widget.opacityCubit!.hide();
-    }
-    Future.delayed(popDelay, () {
-      _overlayEntry?.remove();
-      Navigator.of(context).pop(result == null ? null : result as T);
-    });
+    poppedResult = (result == null ? null : result as T);
+    controller.reverse(); //Start fade-out
   }
 
   void _updatePosition(_) {
-    // Get the position of the ElevatedButton
+    /// Get the position of the calling widget from the key
     final RenderBox renderBox =
         widget.triggerKey.currentContext!.findRenderObject() as RenderBox;
     final Offset widgetPosition = renderBox.localToGlobal(Offset.zero);
 
-    // Create an OverlayEntry to display your child widget
-    _overlayEntry = OverlayEntry(
+    /// Create an OverlayEntry to display the child widget
+    overlayEntry = OverlayEntry(
       builder: (context) => Positioned(
         left: widgetPosition.dx + widget.offset.dx,
         top: widgetPosition.dy + widget.offset.dy,
-        child: widget.builder(context, _pop),
+        child: AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) {
+            return Opacity(
+              opacity: animation.value,
+              child: widget.builder(context, _pop),
+            );
+          },
+        ),
       ),
     );
 
-    // Insert the OverlayEntry into the overlay
-    Overlay.of(context).insert(_overlayEntry!);
+    /// Insert the OverlayEntry into the overlay
+    Overlay.of(context).insert(overlayEntry!);
+    controller.forward(); //Start fade-in
   }
 }
